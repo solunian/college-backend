2de43ddcb1683a30443f41703f04f58927b43d72
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2023-03-26 14:27:29 -0700

    hehehehaqw

--- app.js
+++ app.js
@@ -39,6 +39,29 @@ app.post('/post', (req, res) => {
 	controller.putPost(post);
 });
 
+app.get("/test", (req, res) => {
+	console.log("'/test' API called");
+	controller.test();
+});
+
+app.get("/user/:email", async (req, res) => {
+    console.log("'GET /user:email' called");
+    const user = await controller.getUserByEmail(req.params.email);
+    return res.status(200).json(user);
+})
+
+app.post("/user", async (req, res) => {
+    console.log("'POST /user called");
+    const user = req.body.user;
+    if (user == undefined) return;
+
+    await controller.addUser(user);
+    res.status(200).json();
+})
+
+app.get("/users", (req, res) => {
+    controller.getUsers();
+})
 
 /* Start */ 
 app.listen(2023, () => {
--- controller.js
+++ controller.js
@@ -31,8 +31,92 @@ async function putPost (post) {
 	postsRef.push(post);
 }
 
+async function getUsers() {
+	return usersRef.limitToLast(1).once("value")
+	.then(snapshot => {
+		const posts = snapshot.val();
+		console.log("users at 'getUsers'", posts);
+		return posts;
+	}).catch(e => {
+		console.log(e);
+		console.error("Error on 'getUsers'", e);
+	})
+}
+
+/* USER ROUTES */
+
+//Get user by email - params (email)
+async function getUserByEmail(email) {
+	console.log(email);
+	return (
+		usersRef.orderByChild('email').equalTo(email).limitToLast(1).once("value")
+		.then ((snapshot) => {
+			const val = snapshot.val();
+			const id = Object.keys(val)[0];
+
+			return val[id];
+		}).catch((err) => {
+			return undefined;
+		})
+	)
+}
+
+async function validateUser(user) {
+	return undefined;
+}
+
+
+async function addUser(user) {
+	console.log(user);
+	let err = await validateUser(user);
+
+	if (err == undefined) {
+		let newUser = {
+			name: user.name,
+			tags: user.tags,
+			majors: user.majors,
+			email: user.email
+		};
+
+		if (!newUser.email || !newUser.name) {
+			return;
+		}
+
+		usersRef.push(newUser);
+	} else {
+		console.log("Invalid user.");
+		return err;
+	}
+}
+
+async function addTag(email, tag) {
+	await usersRef.orderByChild('email').equalTo(email).limitToLast(1).once("value", snapshot => {
+		const val = snapshot.val();
+		const id = Object.keys(val)[0];
+		if (val[id].tags == undefined) val[id].tags = [];
+		val[id].tags.push(tag);
+		snapshot.ref.update(val);
+		console.log(`${tag} added to ${email}`);
+	});
+}
+
+async function removeTag(email, tag) {
+	await usersRef.orderByChild('email').equalTo(email).limitToLast(1).once("value", snapshot => {
+		const val = snapshot.val();
+		const id = Object.keys(val)[0];
+		if (val[id].tags == undefined) val[id].tags = [];
+		val[id].tags = _.remove(val[id].tags, function(n) { return n !== tag;});
+		console.log(`${tag} removed from ${email}`);
+	})
+}
+
 module.exports = {
 	test: test,
+	getUserByEmail: getUserByEmail, 
+	addUser: addUser,
+	addTag: addTag,
+	removeTag: removeTag,
 	getPosts: getPosts,
 	putPost: putPost,
+	getUsers: getUsers
 }
