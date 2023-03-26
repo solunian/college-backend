7f9b78d224c91eed4e677343b906fce72140934e
Bhargav Eranki <77950550+beranki@users.noreply.github.com>
2023-03-26 12:58:37 -0700

    pull

--- index.js
+++ index.js
@@ -1,59 +1,19 @@
-import * as express from 'express';
-import * as functions from 'firebase-functions';
-import * as admin from 'firebase-admin';
-import * as bodyParser from "body-parser";
-import {initializeApp} from 'firebase/app';
+const express = require('express');
+require('dotenv').config("/.env");
+const db = require('./db.js');
+const profRef = db.ref('profiles');
+const postsRef = db.ref('posts');
+const port = process.env.port;
 
+const app = express();
 
-const firebaseConfig = {
-  //put firebase project config
-}
+app.get("/", (req, res) => {
+  res.send('herehehehehaw');
+})
 
-const firebaseApp = initializeApp(firebaseConfig);
-
-
-const userCollection = 'users';
-const postCollection = 'posts';
-
+const userRef = db.ref('users');
 /* USER ROUTES */
-app.post('/users', async (req, res) => {
-  try {
-    const user = {
-      name: req.body['name'],
-      tags: req.body['tags'],
-      majors: req.body['majors']
-    }
 
-    const newUser = await db.collection(userCollection).add(user);
-    res.status(201).send(`Created a new user: ${newUser.id}`);
-  } catch (error) {
-    res.status(404).send(`Name, tag, or majors for new user were invalid.`)
-  }
-})
-
-app.get('/users', async (req, res) => {
-  try {
-    const snapshot = await db.collection(userCollection).get();
-    const users = [];
-
-    snapshot.map((user) => {
-      users.push({
-        id: user.id,
-        data: user.data()
-      })
-    });
-
-    res.status(200).json(users);
-  } catch (err) {
-    res.status(500).send(err);
-  }
-})
-
-app.get('/users/:name', (req, res) => {
-  const Name = req.params.name;
-  db.collection(userCollection).doc(id).get()
-  .then(user => {
-    if (!user.exists) throw new Error('User not found.')
-    res.status(200).json({id:user.id, data:user.data()})
-  }).catch(err => res.status(500).send(err));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
 })
